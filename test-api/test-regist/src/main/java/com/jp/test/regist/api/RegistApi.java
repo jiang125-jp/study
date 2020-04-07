419babcdb5645d98be283f4ff5fd4240a9078714
154246581@qq.com <154246581jp>
2020-04-07 18:38:56 +0800

    registApi

--- test-api/test-regist/src/main/java/com/jp/test/regist/api/RegistApi.java
+++ test-api/test-regist/src/main/java/com/jp/test/regist/api/RegistApi.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class registApi {
+public class RegistApi {
 
     @RequestMapping("/hello")
     public String hello() {
