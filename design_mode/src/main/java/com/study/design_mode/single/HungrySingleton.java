be385f0ef079432eb91a58d973582916cc4df20e
jiangpeng <peng.jiang@hoo.com>
2022-04-22 11:29:39 +0400

    学习-设计模式-单例-饿汉模式

--- design_mode/src/main/java/com/study/design_mode/single/HungrySingleton.java
+++ design_mode/src/main/java/com/study/design_mode/single/HungrySingleton.java
@@ -2,7 +2,20 @@ package com.study.design_mode.single;
 
 /**
  * 单例模式-饿汉式
+ *
  * @author jiangpeng
+ *
+ * 优点：执行效率高、性能高、没有锁
+ * 缺点：内存浪费，初始化的时候就加载了
  */
 public class HungrySingleton {
+
+    public static final HungrySingleton HUNGRY_SINGLETON = new HungrySingleton();
+
+    private HungrySingleton() {
+    }
+
+    public static HungrySingleton getInstance() {
+        return HUNGRY_SINGLETON;
+    }
 }
